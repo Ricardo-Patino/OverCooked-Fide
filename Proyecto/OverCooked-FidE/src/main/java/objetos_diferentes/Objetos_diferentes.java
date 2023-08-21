a851971c09622fbdc72e3a2b13cbf87a98844785
Rick Patiño <77856968+Ricardo-Patino@users.noreply.github.com>
2023-08-20 22:41:14 -0600

    Update Objetos_diferentes.java

--- Proyecto/OverCooked-FidE/src/main/java/objetos_diferentes/Objetos_diferentes.java
+++ Proyecto/OverCooked-FidE/src/main/java/objetos_diferentes/Objetos_diferentes.java
@@ -2,6 +2,11 @@ package objetos_diferentes;
 
 import java.awt.image.BufferedImage;
 
+/**
+ *
+ * @author Racha
+ */
+
 public class Objetos_diferentes {
     int x, y;
     BufferedImage reloj_1, reloj_2, reloj_3 , reloj_4 , reloj_5 , reloj_6 , reloj_7;
